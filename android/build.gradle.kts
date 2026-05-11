5e2e95c826c4e28544dafb28f026b8a56ea098c7
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-11 15:50:30 +0530

    code updated

---
+++ android/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+}
+
+group = "com.example.flutter_video_info"
+version = "1.0"
+
+repositories {
+        google()
+        mavenCentral()
+    }
+
+android {
+    namespace = "com.example.flutter_video_info"
+    compileSdk 37
+
+    defaultConfig {
+        minSdk 24
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+    }
+    lintOptions {
+        disable 'InvalidPackage'
+    }
+}
